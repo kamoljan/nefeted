277e0cc53327fd49c0d8732a8f04b7680ca8b844
Kamol Mavlonov <kamol@701search.com>
2014-01-31 20:09:03 +0800

    Code refactoring

--- ad/ad.go
+++ ad/ad.go
@@ -1,13 +1,26 @@
 package ad
 
 import (
-	// "fmt"
+	"fmt"
 	"labix.org/v2/mgo"
-	// "labix.org/v2/mgo/bson"
-	//"time"
+	"labix.org/v2/mgo/bson"
+	"time"
 )
 
-func Save() error {
+type Ad struct {
+	Profile     uint64 // Facebook profile ID
+	Image       string
+	Thumb       string
+	Title       string
+	Category    uint64
+	Description string
+	Price       uint64
+	Currency    string
+	Report      uint64
+	Date        time.Time
+}
+
+func (a *Ad) Save() error {
 	session, err := mgo.Dial("mongodb://admin:12345678@localhost:27017/sa")
 	if err != nil {
 		panic(err)
@@ -17,7 +30,18 @@ func Save() error {
 	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
 
-	// c := session.DB("sa").C("ad")
-	// err := c.Insert(&Ad{r.})
+	c := session.DB("sa").C("ad")
+	err = c.Insert(&a)
+	if err != nil {
+		panic(err)
+	}
+
+	result := Ad{}
+	err = c.Find(bson.M{"title": "test"}).One(&result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Description lah:", result.Description)
+
 	return nil //XXX tmp
 }
--- nefeted.go
+++ nefeted.go
@@ -8,64 +8,17 @@ import (
 	"strconv"
 	"time"
 
-	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
-
-	// "github.com/kamoljan/nefeted/ad"
+	"github.com/kamoljan/nefeted/ad"
 )
 
-type Ad struct {
-	Profile     uint64 // Facebook profile ID
-	Image       string
-	Thumb       string
-	Title       string
-	Category    uint64
-	Description string
-	Price       uint64
-	Currency    string
-	Report      uint64
-	Date        time.Time
-}
-
-func (a *Ad) save() error {
-	// filename := "data/" + p.Title + ".txt"
-	// return ioutil.WriteFile(filename, p.Body, 0600)
-	session, err := mgo.Dial("mongodb://admin:12345678@localhost:27017/sa")
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-
-	c := session.DB("sa").C("ad")
-	err = c.Insert(&a)
-	if err != nil {
-		panic(err)
-	}
-
-	result := Ad{}
-	err = c.Find(bson.M{"title": "test"}).One(&result)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Description lah:", result.Description)
-
-	return nil //XXX tmp
-}
-
-// func Message(status string, message string, params []string) []byte {
 func Message(status string, message string) []byte {
 	type Message struct {
 		Status  string
 		Message string
-		//Params  []string
 	}
 	m := Message{
 		Status:  status,
 		Message: message,
-		//Params:  params,
 	}
 	b, err := json.Marshal(m)
 	if err != nil {
@@ -121,7 +74,7 @@ func adHandler(w http.ResponseWriter, r *http.Request, fid string) {
 			w.Write(Message("Error", "Currency is missing"))
 			return
 		}
-		ad := &Ad{
+		ad := &ad.Ad{
 			Profile:     profile,
 			Image:       image,
 			Thumb:       thumb,
@@ -133,13 +86,11 @@ func adHandler(w http.ResponseWriter, r *http.Request, fid string) {
 			Report:      0,
 			Date:        time.Now(),
 		}
-
-		err = ad.save()
+		err = ad.Save()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
 		w.Write(Message("OK", "Saved!"))
 	} else {
 		http.NotFound(w, r)
