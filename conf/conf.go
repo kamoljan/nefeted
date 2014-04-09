15a25bd608783990a0e8768372dbeae31406acd8
Kamol Mavlonov <kamol@701search.com>
2014-04-09 17:37:23 +0800

    LISTING: Configurable URL for Ikura

--- conf/conf.go
+++ conf/conf.go
@@ -2,7 +2,7 @@ package conf
 
 const (
 	Mongodb     = "mongodb://admin:12345678@localhost:27017/sa"
-	IkuraUrl    = "http://nefete.com:9090/"
+	IkuraUrl    = "http://nefete.com:9090/egg/"
 	NefetedPort = 8080
 	ResultLimit = 20
 )
